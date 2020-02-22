9cbf782ed06ed3174ea7ef369b8f82efa3609290
YashBhartia00 <51514725+YashBhartia00@users.noreply.github.com>
2020-02-22 11:40:55 +0530

    initial comit before somthing breaks

---
+++ dots And Boxes/Assets/ForTTT/test.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Models;
+using Proyecto26;
+using UnityEngine.Networking;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class test : MonoBehaviour
+{
+    public int userNumber, drawNumber;
+    public Text gameType, userName, youName, enemyName;
+    public static int playerNumber = 1;
+    public static int[] positions = new int[10];
+    public GameObject[] pcsX;
+    public GameObject[] pcsO;
+    public board thisBoard = new board(positions);
+    public bool lookingForMatch, draw;
+    int[,] winCombos = new int[8, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 2, 4, 6 } };
+    public int[] check = new int[10];
+
+    public GameObject[] disableOnPlay, enableOnPlay, winLose, postitionsObjs; //enableOnPlay 0 is positions
+
+    void Start()
+    {
+        int[] positions = new int[10];
+        for (int i = 0; i < winLose.Length; i++) { winLose[i].SetActive(false); print("bl"); } 
+        print("Player number = 1 or 2, 1 by default");
+        InvokeRepeating("live", 1f, 0.5f);
+    }
+
+    public void lookForBoard()
+    { userNumber = getTime();
+
+        for (int i = 0; i < disableOnPlay.Length; i++)
+        {
+            disableOnPlay[i].SetActive(false);
+        }
+        Matches newMatch = new Matches(gameType.text.ToString(), userName.text.ToString(), thisBoard, userNumber);
+
+        RestClient.Get<Matches>("https://dots-68b2c.firebaseio.com/Pendings/" + newMatch.gameType + "/1/" + ".json").Then(res =>
+              {
+                  youName.text = "O: " + userName.text;
+                  RestClient.Put("https://dots-68b2c.firebaseio.com/Pendings/" + newMatch.gameType + "/1/" + ".json", newMatch);
+                  //print(res.userName);
+                  enemyName.text = "X: " + res.userName;
+                  newMatch.found = true;
+                  userNumber = res.timeCreated;
+                  RestClient.Put("https://dots-68b2c.firebaseio.com/Playings/" + newMatch.gameType + "/" + res.timeCreated + ".json", thisBoard);
+                  OnPlay();
+                  playerNumber = 2;
+              }).Finally(() => createMatch(newMatch.found, newMatch));
+
+    }
+
+    public void createMatch(bool execute, Matches thisMatch)
+    {
+        if (!execute)
+        {
+            lookingForMatch = true;
+            RestClient.Put("https://dots-68b2c.firebaseio.com/Pendings/" + thisMatch.gameType + "/" + 1 + ".json", thisMatch);
+            youName.text = "X: " + userName.text;
+        }
+    }
+
+    private void Update()
+    {
+        plotBoard();
+        gameRefree();
+        if (lookingForMatch)
+        {
+            RestClient.Get<board>("https://dots-68b2c.firebaseio.com/Playings/" + gameType.text.ToString() + "/" + userNumber + ".json").Then(res =>
+            { lookingForMatch = false;
+                OnPlay();
+                playerNumber = 1;
+                RestClient.Get<Matches>("https://dots-68b2c.firebaseio.com/Pendings/" + gameType.text.ToString() + "/1/" + ".json").Then(resu =>
+                {
+                    //print(rest.userName)
+                    enemyName.text = "O: " + resu.userName;
+                    RestClient.Delete("https://dots-68b2c.firebaseio.com/Pendings/" + gameType.text.ToString() + "/1/" + ".json");
+                });
+
+            });
+        }
+        check = positions;
+
+    }
+
+    public board live()
+    {
+        if (!lookingForMatch)
+        {
+            RestClient.Get<board>("https://dots-68b2c.firebaseio.com/Playings/" + gameType.text.ToString() + "/" + userNumber + ".json").Then(res =>
+            { positions = res.positions; });
+        }
+        return (new board(positions));
+    }
+
+    public void OnPlay()
+    {
+
+        for (int i = 0; i < enableOnPlay.Length; i++)
+        {
+            enableOnPlay[i].SetActive(true);
+        }
+
+    }
+
+    public void plotBoard()
+    {
+        drawNumber = 0;
+        for (int i = 0; i < 9; i++)
+        {
+            pcsX[i].SetActive(false);
+            pcsO[i].SetActive(false);
+
+            if (positions[i] == 1)
+            {
+                pcsX[i].SetActive(true);
+                drawNumber++;
+            }
+            else if (positions[i] == 2)
+            {
+                pcsO[i].SetActive(true);
+                drawNumber++;
+            }
+        }
+        if (drawNumber == 9)
+        {
+            draw = true;
+        }
+    }
+
+    public void gameRefree()
+    {
+        if (playerNumber == positions[9]) { changeBox(false); } else { changeBox(true); }
+        if (checkWin() != 0)
+        {
+            //enableOnPlay[0].SetActive(false);
+            changeBox(false);
+            winLose[4].SetActive(true);
+            if (checkWin() == 3) { winLose[2].SetActive(true); } else if (playerNumber == checkWin()) { winLose[0].SetActive(true); } else { winLose[1].SetActive(true); }
+            winLose[3].SetActive(true);
+            
+
+        }
+
+    }
+    public void changeBox(bool active)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            postitionsObjs[i].GetComponent<BoxCollider2D>().enabled = active;
+        }
+    }
+    public int checkWin()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if (positions[winCombos[i, 0]] == positions[winCombos[i, 1]] && positions[winCombos[i, 1]] == positions[winCombos[i, 2]]) {
+                return positions[winCombos[i, 0]];
+            }
+        }
+        if (draw) { return 3; }
+        return 0;
+    }
+    public int getTime()
+    {
+        System.DateTime epochStart = new System.DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
+        int cur_time = (int)(System.DateTime.UtcNow - epochStart).TotalSeconds;
+        return cur_time;
+
+    }
+
+    public void restart()
+    {
+        //for (int i = 0; i < 4; i++) { if (winLose[i].activeSelf) { winLose[i].SetActive(false); print("bl"); } }
+        SceneManager.LoadScene("StartScreen");
+        
+    }
+}
---
+++ dots And Boxes/Assets/MainDots.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainDots : MonoBehaviour
+{
+    public GameObject dotPrefab, dotParent, linePrefab, lineParent, boxPrefab, boxParent;
+    GameObject[,,] gridLines = new GameObject[2, 11, 9]; //1st for H[0] or V[1], (0,0) startingform leftbottom
+    public int[] gridSize = new int[2] { 5, 4 };
+    public boardDots thisBoard = new boardDots(new int[2, 2, 2], 1);
+    SpriteRenderer[,] boxes = new SpriteRenderer[1, 1];
+    public int playerNumber;
+    public bool move;
+
+    //Color blue, red, violet, green,blank;
+    public static Color[] BRVG = new Color[5];
+
+    private void Start()
+    {
+        gridLines = new GameObject[2, 2 * gridSize[0] + 1, 2 * gridSize[1] + 1];
+        thisBoard = new boardDots(new int[2, 2 * gridSize[0] + 1, 2 * gridSize[1] + 1], 2);
+        boxes = new SpriteRenderer[gridSize[0] * 2, gridSize[1] * 2];
+        initializeColors();
+        makeBoard(gridSize);
+        InvokeRepeating("plotBoardLines", 1, 2);
+    }
+    private void Update()
+    {
+        playerNumber = Input.GetKeyDown(KeyCode.A) ? 1 : Input.GetKeyDown(KeyCode.B) ? 2 : Input.GetKeyDown(KeyCode.C) ? 3 : Input.GetKeyDown(KeyCode.D) ? 4 : 0;
+        print(playerNumber);
+    }
+    void initializeColors()
+    {
+        ColorUtility.TryParseHtmlString("#0048FF", out BRVG[0]);
+        ColorUtility.TryParseHtmlString("#FF002D", out BRVG[1]);
+        ColorUtility.TryParseHtmlString("#F300FF", out BRVG[2]);
+        ColorUtility.TryParseHtmlString("#00CC1F", out BRVG[3]);
+        ColorUtility.TryParseHtmlString("#FFFFFF", out BRVG[3]);
+        BRVG[0].a = 0.5f;
+        BRVG[1].a = 0.5f;
+        BRVG[2].a = 0.5f;
+        BRVG[3].a = 0.5f;
+        BRVG[4].a = 0f;
+    }
+
+    void makeBoard(int[] gs)
+    {
+        Vector3 temp = new Vector3(0, thisBoard.gridLine.GetLength(1) - 1, thisBoard.gridLine.GetLength(2) - 1);
+        for (int i = -gs[0]; i <= gs[0]; i++)
+        {
+            for (int j = -gs[1]; j <= gs[1]; j++)
+            {
+                Instantiate(dotPrefab, new Vector3(i, j, 0), Quaternion.identity, dotParent.transform);
+
+            }
+        } //for dots
+        for (int i = -gs[0]; i < gs[0]; i++) //for H
+        {
+            for (int j = -gs[1]; j <= gs[1]; j++)
+            {
+                var a = Instantiate(linePrefab, new Vector3(i + 0.5f, j, 0), Quaternion.identity, lineParent.transform);
+                a.transform.up = new Vector2(1, 0);
+                gridLines[0, i + gs[0], j + gs[1]] = a;
+            }
+        } //for H
+        for (int i = -gs[0]; i <= gs[0]; i++) //for V
+        {
+            for (int j = -gs[1]; j < gs[1]; j++)
+            {
+                var a = Instantiate(linePrefab, new Vector3(i, j + 0.5f, 0), Quaternion.identity, lineParent.transform);
+                gridLines[1, i + gs[0], j + gs[1]] = a;
+            }
+        } //for V
+        for (int i = 0; i < temp.y; i++)
+        {
+            for (int j = 0; j < temp.z; j++)
+            {
+                var a = Instantiate(boxPrefab, new Vector3(i - 5 + 0.5f, j - 4 + 0.5f, 0), Quaternion.identity, boxParent.transform);
+                boxes[i, j] = a.GetComponent<SpriteRenderer>();
+                boxes[i, j].color = BRVG[4];
+            }
+        }
+
+    }
+    void plotBoardLines()
+    {
+        thisBoard.randomize();
+        for (int j = 0; j + 1 < thisBoard.gridLine.GetLength(1); j++)
+        {
+            for (int k = 0; k < thisBoard.gridLine.GetLength(2); k++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    gridLines[0, j, k].transform.GetChild(i).gameObject.SetActive(false);
+                }
+                gridLines[0, j, k].transform.GetChild(thisBoard.gridLine[0, j, k] - 1).gameObject.SetActive(true);
+            }
+        }
+        for (int j = 0; j < thisBoard.gridLine.GetLength(1); j++)
+        {
+            for (int k = 0; k + 1 < thisBoard.gridLine.GetLength(2); k++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    gridLines[1, j, k].transform.GetChild(i).gameObject.SetActive(false);
+                }
+                gridLines[1, j, k].transform.GetChild(thisBoard.gridLine[1, j, k] - 1).gameObject.SetActive(true);
+            }
+        }
+        plotBoxes();
+    }
+    void plotBoxes()
+    {
+        Vector3 temps = new Vector3(0, thisBoard.gridLine.GetLength(1) - 1, thisBoard.gridLine.GetLength(2) - 1);
+        for (int i = 0; i < temps.y; i++)
+        {
+            for (int j = 0; j < temps.z; j++)
+            {
+                boxes[i, j].color = BRVG[4];
+                if (thisBoard.gridLine[0, i, j] == thisBoard.gridLine[0, i, j + 1])
+                {
+                    if (thisBoard.gridLine[1, i, j] == thisBoard.gridLine[1, i + 1, j])
+                    {
+                        if (thisBoard.gridLine[0, i, j] == thisBoard.gridLine[1, i, j])
+                        {
+
+                            boxes[i, j].color = BRVG[thisBoard.gridLine[0, i, j] - 1];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
